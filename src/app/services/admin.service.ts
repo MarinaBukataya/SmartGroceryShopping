14a9965ca38502ffdf2fde929073f68909e44234
Marina Bukataya <ma.bukataya@gmail.com>
2021-04-21 15:47:04 +0300

    add

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -12,7 +12,7 @@ import { LoginResponse } from '../models/LoginResponse';
   providedIn: 'root'
 })
 export class AdminService {
-  //private BASE_URL = 'https://encoded-breaker-309609.ew.r.appspot.com/admin'
+ // private BASE_URL = 'https://encoded-breaker-309609.ew.r.appspot.com/admin'
   private BASE_URL = 'http://localhost:8080/admin'
 
   constructor(private httpClient: HttpClient) { }
